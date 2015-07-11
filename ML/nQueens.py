a9fe310300b3476f022b61085498bc898f1d422f
michpasq <michpasq@umich.edu>
2015-07-11 18:12:38 -0400

    Moving nQueens.py

--- ML/nQueens.py
+++
@@ -1,4 +0,0 @@
-# Try a greedy search approach to N Queens problem.
-# Source - https://sites.google.com/site/nqueensolver/home/algorithms/3systematic-greedy-algorithm
-#
-# Note the importance of initialization!
