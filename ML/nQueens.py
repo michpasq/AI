3e67ed54ed8d6f9279a4b6b0df52ab7fc51ce2c2
michpasq <michpasq@umich.edu>
2015-07-11 10:53:21 -0400

    First problem to try for ML: N Queens

---
+++ ML/nQueens.py
@@ -0,0 +1,4 @@
+# Try a greedy search approach to N Queens problem.
+# Source - https://sites.google.com/site/nqueensolver/home/algorithms/3systematic-greedy-algorithm
+#
+# Note the importance of initialization!
